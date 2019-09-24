204527200a1729fe0e0f08c4b20f99e390d5bacf
Jimmique <jimmique88@gmail.com>
2019-09-23 20:58:24 -0700

    Initial commit (created h1 and div in App.js)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>NASA Photo of the Day</h1>
+      <div className="main-container">
+      </div>
     </div>
   );
 }
