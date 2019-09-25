96ea43b86c6d298a5f01e19e7aaff01dce424ea6
Jimmique <jimmique88@gmail.com>
2019-09-24 22:31:03 -0700

    installed a scrollbar dependency, and styled image text

--- src/components/ImageText.js
+++ src/components/ImageText.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Scrollbar } from "react-scrollbars-custom";
 import "./imageText.css";
 
 const ImageText = ({ title, exp }) => {
@@ -6,7 +7,9 @@ const ImageText = ({ title, exp }) => {
         <div className="main-text-div">
             <h3>{title}</h3>
             <div className="inner-text-div">
+              <Scrollbar style={{ width: "100%", height: "100%" }}>
                 <p>{exp}</p>
+              </Scrollbar>  
             </div>
         </div>
     );
